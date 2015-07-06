26138413e889632638249d9bf9b5e4f088adc634
Arfon Smith <arfon@github.com>
2015-07-06 15:47:11 +0100

    Basic author parsing

--- lib/metamatter.rb
+++ lib/metamatter.rb
@@ -10,8 +10,24 @@ module Metamatter
     def extract(repo_with_owner)
       # Do something like...
       # Get contributors
+      authors = contributors(repo_with_owner)
       # Get the readme
       # To some LDA analysis on the README to work out what this project is doing
+    end
+
+    def contributors(repo_with_owner)
+      authors = []
+      contribs = contributors_response(repo_with_owner)
+      contribs.each do |c|
+        user = client.user(c.login)
+        authors << {:name => user.name, :email => user.email, :login => user.login, :contributions => c.contributions}
+      end
+
+      return authors.sort_by { |a| a[:contributions] }.reverse
+    end
+
+    def contributors_response(repo_with_owner)
+      @response ||= client.contributors(repo_with_owner)
     end
   end
 end
