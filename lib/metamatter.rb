87af6aa89852251c77c7a2fb94d1755d0c07ff67
Arfon Smith <arfon@github.com>
2015-07-07 14:47:37 +0100

    No more class methods

--- lib/metamatter.rb
+++ lib/metamatter.rb
@@ -3,13 +3,18 @@ require_relative 'metamatter/classification'
 require_relative 'metamatter/contributors'
 
 module Metamatter
-  class << self
-    def extract(repo_with_owner)
-      owner, repo = repo_with_owner.split('/')
+  class Repository
+    attr_accessor :owner
+    attr_accessor :repository
 
-      authors = Contributors.new(:repository => repo, :owner => owner).list
+    def initialize(repo_with_owner)
+      @owner, @repository = repo_with_owner.split('/')
+    end
+
+    def extract
+      authors = Contributors.new(:repository => repository, :owner => owner).list
       # To some LDA analysis on the README to work out what this project is doing
-      tags = Classification.new(:repository => repo, :owner => owner).tags
+      tags = Classification.new(:repository => repository, :owner => owner).tags
 
       return JSON.pretty_generate({:authors => authors, :tags => tags})
     end
