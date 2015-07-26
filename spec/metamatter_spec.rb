df8b16d9d94b38f8203efed40ee2abadf5209ec7
Arfon Smith <arfon@github.com>
2015-07-26 08:33:24 +0100

    Adding some more tests to base class

--- spec/metamatter_spec.rb
+++ spec/metamatter_spec.rb
@@ -7,4 +7,28 @@ describe Metamatter do
   it "knows how to return #name_with_owner" do
     expect(subject.name_with_owner).to eql('arfon/metamatter')
   end
+
+  it "can return #authors" do
+    VCR.use_cassette('authors') do
+      expect(subject.authors.first[:login]).to eql('arfon')
+    end
+  end
+
+  it "can return a #tags" do
+    VCR.use_cassette('algorithmia') do
+      expect(subject.tags).to eql(["output", "blog", "context", "usage", "entities", "post", "similar", "semantic"])
+    end
+  end
+
+  it "can return #license" do
+    VCR.use_cassette('license') do
+      expect(subject.license[:name]).to eql('MIT License')
+    end
+  end
+
+  it "can return #doi" do
+    VCR.use_cassette('readme') do
+      expect(subject.doi).to eql("http://dx.doi.org/10.5281/zenodo.19630")
+    end
+  end
 end
